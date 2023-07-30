80c64e42b8e96f3358369a7e3c885ee2eacc4358
altayavci <altayavci2000@gmail.com>
2023-07-30 03:33:30 +0300

    utils

---
+++ gzip-knn/utils.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd 
+import seaborn as sns 
+
+from sklearn.metrics import (confusion_matrix, 
+    classification_report, 
+    ConfusionMatrixDisplay
+)
+
+def plot_confusion_matrix(val_y, pred, classes, save_img):
+    plt.figure(figsize = (15, 13), facecolor = 'silver', edgecolor = 'gray')
+
+    cm = confusion_matrix(val_y, pred, labels=classes)
+
+    ax= plt.subplot()
+    sns.heatmap(cm, annot=True, fmt='g', ax=ax)
+    ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels')
+    ax.set_title('Confusion Matrix')
+    ax.xaxis.set_ticklabels(classes); ax.yaxis.set_ticklabels(classes)
+    plt.savefig(save_img)
+
+
+def plot_classification_report(val_y, pred, classes, save_img):
+    plt.figure(figsize = (15, 15), facecolor = 'silver', edgecolor = 'gray')
+
+    cr = classification_report(val_y, pred,
+                                   target_names = classes,
+                                   output_dict = True)
+                                   
+    sns.heatmap(pd.DataFrame(cr).iloc[:-1, :].T, annot=True)
+    plt.savefig(save_img)
